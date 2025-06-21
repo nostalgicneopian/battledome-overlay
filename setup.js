5527e678f9a5b6412802416d6eb35797b5e66207
Jose Cabrera <jcabben@outlook.com>
2025-06-21 15:16:11 -0700

    Added basic file structure

---
+++ setup.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Neopets Battledome Overlay
+// @namespace   neopets
+// @version     0.0.0
+// @description Quality of Life improvements to the Battledome
+// @author      nostalgicneopian
+// @match       *://*.neopets.com/dome/*
+// @icon        https://images.neopets.com/themes/031_skr_8944c/events/battle_challenge.png
+// @require     
+// @grant       none
+// ==/UserScript==
