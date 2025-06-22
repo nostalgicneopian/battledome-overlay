b0a7a28b2f8ca2be190f76ca5cef5bbbd94e39c0
Jose Cabrera <jcabben@outlook.com>
2025-06-21 21:10:33 -0700

    Added GM permissions to setup

--- setup.js
+++ setup.js
@@ -7,5 +7,6 @@
 // @match       *://*.neopets.com/dome/*
 // @icon        https://images.neopets.com/themes/031_skr_8944c/events/battle_challenge.png
 // @require     https://raw.githubusercontent.com/nostalgicneopian/battledome-overlay/refs/heads/feature/expanded-battle-log/src/expanded-log-module.js
-// @grant       none
+// @grant       GM_getvalue
+// @grant       GM_setValue
 // ==/UserScript==
