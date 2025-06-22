8d41786792576e550ea927e6f32ea0c2889bea72
Jose Cabrera <jcabben@outlook.com>
2025-06-21 21:05:27 -0700

    Added private branch file path on setup for testing

--- setup.js
+++ setup.js
@@ -6,6 +6,6 @@
 // @author      nostalgicneopian
 // @match       *://*.neopets.com/dome/*
 // @icon        https://images.neopets.com/themes/031_skr_8944c/events/battle_challenge.png
-// @require     
+// @require     https://raw.githubusercontent.com/nostalgicneopian/battledome-overlay/refs/heads/feature/expanded-battle-log/src/expanded-log-module.js
 // @grant       none
 // ==/UserScript==
